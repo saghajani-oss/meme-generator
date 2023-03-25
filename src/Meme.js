df5761db698734052b0b76f17fc9468043663ff9
saghajani-oss <112084533+saghajani-oss@users.noreply.github.com>
2023-03-25 18:11:35 +0430

    text on Image

--- src/Meme.js
+++ src/Meme.js
@@ -1,33 +1,58 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import memesData from "./memesData";
 
 export default function Meme() {
-  // let [memeImage, setMemeImage] = useState("");
   const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
     randomImage: "https://i.imgflip.com/1bij.jpg",
   });
 
-  const [allMemeImages, setAllMemeImages] = useState(memesData)
+  console.log(meme)
+
+  const [allMemeImages, setAllMemeImages] = useState(memesData);
 
   function memeImgGenerate() {
     const memeArray = allMemeImages.data.memes;
     const randomNumber = Math.floor(Math.random() * memeArray.length);
     const imageUrl = memeArray[randomNumber].url;
-    setMeme(prevMeme => ({
+    setMeme((prevMeme) => ({
       ...prevMeme,
-      randomImage: imageUrl
+      randomImage: imageUrl,
     }));
   }
-    return (
-      <main className="meme">
-        <div>
-          <input type="text" placeholder="top text" />
-          <input type="text" placeholder="button text" />
-          <button onClick={memeImgGenerate}>Get a new meme image 🖼</button>
-        </div>
-        <img src={meme.randomImage} alt="meme-img" />
-      </main>
-    );
+
+  function handleTextChange(event) {
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+
+  return (
+    <main className="meme">
+      <div>
+        <input
+          type="text"
+          placeholder="top text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleTextChange}
+        />
+        <input
+          type="text"
+          placeholder="button text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleTextChange}
+        />
+        <button onClick={memeImgGenerate}>Get a new meme image 🖼</button>
+      </div>
+      <img src={meme.randomImage} alt="meme-img" />
+      <h2 className="image-text top">{meme.topText}</h2>
+      <h2 className="image-text bottom">{meme.bottomText}</h2>
+    </main>
+  );
 }
